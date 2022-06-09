e8cf00f9086ced8d48ffcfb971846fb2737d4817
Guilherme Teles <gui-teles2010@hotmail.com>
2022-06-09 17:08:26 -0300

    preparação do ambiente

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+
+      <form>
+        <input type="text" placeholder="Cidade" />
+      </form>
+
+    </div>
+
+  );
+}
+
+export default App;
